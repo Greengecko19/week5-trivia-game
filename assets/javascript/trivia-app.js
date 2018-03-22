0b2f9739a2ce8fa751d5ad99cec6c08739f0ddca
Kamran Grasselli <KGrasselli@Soliel.local>
2018-03-22 17:23:32 -0400

    minor js tweaks, still not working

--- assets/javascript/trivia-app.js
+++ assets/javascript/trivia-app.js
@@ -1,23 +1,32 @@
+// My logic is good, but my code is not:
+// 1. Upon clicking the Start button, clear out selections and start timer
+// 2. Store each clicked answer in a variable
+// 3. When 30 seconds are up, add up the number of correct and incorrect answers
 
-
-
-// window.onload = function() {
-//     $("#start").on("click", startTimer);
-// };
 
 var correct = 0;
 var incorrect = 0;
+var time = 0;
+var clockRunning = false;
 
-function countdown() {
-    setInterval(checkAnswers(), 30000);
-    clearInterval();
-}
+
+$('#start').on("click", function() { 
+    $('input[name="q1"]').attr('checked', false);
+    $('input[name="q2"]').attr('checked', false);
+    $('input[name="q3"]').attr('checked', false);
+    $('input[name="q4"]').attr('checked', false);
+    intervalId = setInterval(30000);
+    $('#timer').text(intervalId);
+    checkAnswers();
+});
 
 function checkAnswers() {
 
-    var answer1 = $('#q1').val();
+    clearInterval(intervalId);
+
+    var answer1 = $('input[name="q1"]').text;
     console.log(answer1);
-    if (answer1 = "Pizza") {
+    if (answer1 == "Pizza") {
         correct++;
     }
     else {
@@ -25,8 +34,8 @@ function checkAnswers() {
         break;
     }
 
-    var answer2 = $('#q2').val();
-    if (answer1 = "Thumb wrestling") {    
+    var answer2 = $('input[name="q2"]').text;
+    if (answer1 == "Thumb wrestling") {    
         correct++;
     }
     else {
@@ -35,28 +44,46 @@ function checkAnswers() {
     }
 
 
-    var answer3 = $('#q3').val();
+    var answer3 = $('input[name="q3"]').text;
     console.log(answer3);
-    if (answer3 = "A Flock of Seagulls") {
-            correct++;
+    if (answer3 == "A Flock of Seagulls") {
+        correct++;
     }
     else {
         incorrect++;
         break;
     }
 
-    var answer4 = $('#q4').val();
+    var answer4 = $('input[name="q4"]').text;
     console.log(answer4);
-    if (answer3 = "Get Smart") {    
-            correct++;
+    if (answer3 == "Get Smart") {    
+        correct++;
     }
     else {
         incorrect++;
         break;
     }
-
-    $('number-correct').text(correct);
-    $('number-incorrect').text(incorrect);
-
+    
 }
+
+function timeConverter() {
+
+    var converted = intervalId.timeConverter(intervalId.time);
+    console.log(converted);
+
+    $("#timer").text(converted);
+
+    function(t) {
+    var seconds = Math.floor(t * 30);
+
+    if (seconds < 31) {
+        seconds = "0" + seconds;
+        return seconds;
+    }
+    else {
+        // display score
+        $('number-correct').text(correct);
+        $('number-incorrect').text(incorrect);
+        break;
+    }
 
